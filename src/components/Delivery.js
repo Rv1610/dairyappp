94871d79832ee3177a42a7529c86b395c468b112
Ravi <rakar2317@gmail.com>
2024-11-06 14:09:23 +0530

    npm build and Delivery para

--- src/components/Delivery.js
+++ src/components/Delivery.js
@@ -4,7 +4,7 @@ function Delivery() {
   return (
     <div>
       <h1>Delivery Services</h1>
-      <p>We deliver fresh dairy products right to your doorstep. Learn more about our delivery services here.</p>
+      <p>We deliver fresh dairy products right to your doorstep. Learn more about our delivery services here. Ap per distance we charge something</p>
     </div>
   );
 }
