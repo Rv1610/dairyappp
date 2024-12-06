04f8251bf7bf0aaae51b05a6cf631d9fa5eebecb
Ravi <rakar2317@gmail.com>
2024-12-06 10:37:36 +0530

    Change the UI for small screen sizes easy to interact for users

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,42 +1,56 @@
-// import React, {useState} from 'react';
+// import React, { useState } from 'react';
 // import { Link } from 'react-router-dom';
 // import '../assets/Styles/Header.css';
 // import dairyLogo from '../assets/Images/DairyLogo-Photoroom.png';
+
 // function Header({ isModalVisible }) {
 //   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
 //   const toggleMenu = () => {
-//     setIsMenuOpen(!isMenuOpen);
+//     if (!isModalVisible) {
+//       setIsMenuOpen(!isMenuOpen);
+//     }
 //   };
-
-
 
 //   return (
 //     <nav>
-//       <div className= "navbar-container">
+//       <div className="navbar-container">
 //         <div className="logo">
 //           <img src={dairyLogo} alt="logo" />
 //         </div>
-//         <ul className={`nav-links ${isMenuOpen ? 'open' : ''}`}>
+//         {/* Disable links when modal is active */}
+//         <ul className={`nav-links ${isMenuOpen ? 'open' : ''} ${isModalVisible ? 'disabled' : ''}`}>
 //           <li>
-//             <Link to="/">Home</Link>
+//             <Link to="/" className={isModalVisible ? 'disabled-link' : ''}>
+//               Home
+//             </Link>
 //           </li>
 //           <li>
-//             <Link to="/products">Product</Link>
+//             <Link to="/products" className={isModalVisible ? 'disabled-link' : ''}>
+//               Product
+//             </Link>
 //           </li>
 //           <li>
-//             <Link to="/delivery">Delivery Services</Link>
+//             <Link to="/delivery" className={isModalVisible ? 'disabled-link' : ''}>
+//               Delivery Services
+//             </Link>
 //           </li>
 //           <li>
-//             <Link to="/contactus">Contact Us</Link>
+//             <Link to="/contactus" className={isModalVisible ? 'disabled-link' : ''}>
+//               Contact Us
+//             </Link>
 //           </li>
-//           <button className="orderButton">
-//             <Link to="/order">Order Now</Link>
+//           <button className={`orderButton ${isModalVisible ? 'disabled-link' : ''}`}>
+//             <Link to="/order" className={isModalVisible ? 'disabled-link' : ''}>
+//               Order Now
+//             </Link>
 //           </button>
 //         </ul>
+//         {/* Disable menu toggle when modal is active */}
 //         <button
-//           className={`hamburger-menu ${isMenuOpen ? 'open' : ''}`}
+//           className={`hamburger-menu ${isMenuOpen ? 'open' : ''} ${isModalVisible ? 'disabled-link' : ''}`}
 //           onClick={toggleMenu}
+//           disabled={isModalVisible}
 //         >
 //           <div className="bar"></div>
 //           <div className="bar"></div>
@@ -50,10 +64,19 @@
 // export default Header;
 
 
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import '../assets/Styles/Header.css';
-import dairyLogo from '../assets/Images/DairyLogo-Photoroom.png';
+
+
+
+
+
+
+
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome, faBoxOpen, faTruck, faPhone, faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import "../assets/Styles/Header.css";
+import dairyLogo from "../assets/Images/DairyLogo-Photoroom.png";
 
 function Header({ isModalVisible }) {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -70,37 +93,45 @@ function Header({ isModalVisible }) {
         <div className="logo">
           <img src={dairyLogo} alt="logo" />
         </div>
-        {/* Disable links when modal is active */}
-        <ul className={`nav-links ${isMenuOpen ? 'open' : ''} ${isModalVisible ? 'disabled' : ''}`}>
+        
+        {/* Large Screen Navigation */}
+        <ul
+          className={`nav-links ${
+            isMenuOpen ? "open" : ""
+          } ${isModalVisible ? "disabled" : ""}`}
+        >
           <li>
-            <Link to="/" className={isModalVisible ? 'disabled-link' : ''}>
+            <Link to="/" className={isModalVisible ? "disabled-link" : ""}>
               Home
             </Link>
           </li>
           <li>
-            <Link to="/products" className={isModalVisible ? 'disabled-link' : ''}>
-              Product
+            <Link to="/products" className={isModalVisible ? "disabled-link" : ""}>
+              Products
             </Link>
           </li>
           <li>
-            <Link to="/delivery" className={isModalVisible ? 'disabled-link' : ''}>
+            <Link to="/delivery" className={isModalVisible ? "disabled-link" : ""}>
               Delivery Services
             </Link>
           </li>
           <li>
-            <Link to="/contactus" className={isModalVisible ? 'disabled-link' : ''}>
+            <Link to="/contactus" className={isModalVisible ? "disabled-link" : ""}>
               Contact Us
             </Link>
           </li>
-          <button className={`orderButton ${isModalVisible ? 'disabled-link' : ''}`}>
-            <Link to="/order" className={isModalVisible ? 'disabled-link' : ''}>
+          <button className={`orderButton ${isModalVisible ? "disabled-link" : ""}`}>
+            <Link to="/order" className={isModalVisible ? "disabled-link" : ""}>
               Order Now
             </Link>
           </button>
         </ul>
-        {/* Disable menu toggle when modal is active */}
+
+        {/* Hamburger Menu for Large Screens */}
         <button
-          className={`hamburger-menu ${isMenuOpen ? 'open' : ''} ${isModalVisible ? 'disabled-link' : ''}`}
+          className={`hamburger-menu ${
+            isMenuOpen ? "open" : ""
+          } ${isModalVisible ? "disabled-link" : ""}`}
           onClick={toggleMenu}
           disabled={isModalVisible}
         >
@@ -108,6 +139,30 @@ function Header({ isModalVisible }) {
           <div className="bar"></div>
           <div className="bar"></div>
         </button>
+      </div>
+
+      {/* Bottom Navigation for Small Screens */}
+      <div className={`bottom-nav ${isModalVisible ? "disabled" : ""}`}>
+        <Link to="/" className={isModalVisible ? "disabled-link" : ""}>
+          <FontAwesomeIcon icon={faHome} />
+          <span>Home</span>
+        </Link>
+        <Link to="/products" className={isModalVisible ? "disabled-link" : ""}>
+          <FontAwesomeIcon icon={faBoxOpen} />
+          <span>Products</span>
+        </Link>
+        <Link to="/delivery" className={isModalVisible ? "disabled-link" : ""}>
+          <FontAwesomeIcon icon={faTruck} />
+          <span>Delivery</span>
+        </Link>
+        <Link to="/contactus" className={isModalVisible ? "disabled-link" : ""}>
+          <FontAwesomeIcon icon={faPhone} />
+          <span>Contact</span>
+        </Link>
+        <Link to="/order" className={isModalVisible ? "disabled-link" : ""}>
+          <FontAwesomeIcon icon={faCartShopping} />
+          <span>Order</span>
+        </Link>
       </div>
     </nav>
   );
